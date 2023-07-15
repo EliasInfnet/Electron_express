e2d6391a998af3d45b3bc9986c4cb35de27d2de4
EliasInfnet <elias.carvalho@al.infnet.edu.br>
2023-07-15 14:06:50 -0300

    first

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    sourceType: 'module',
+    ecmaVersion: 2021
+  }
+}
